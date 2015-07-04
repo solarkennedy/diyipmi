d8345aedfd9bac5b06f486db4e8bcd11c2087a6a
Kyle Anderson <kyle@xkyle.com>
2015-07-03 18:25:46 -0700

    Try to print more stuff...

--- asf.ino
+++ asf.ino
@@ -1,13 +1,17 @@
 
-void respondToASF(const char *data, word len, byte *ip, word port) {
+void respondToASF(const char *data, word len, byte *srcip, word port) {
   Serial.println("Got an ASF packet");
 
   //Hard coded ASF Pong. All I care about really
   char reply[] = { 0x06, 0x00, 0xff, 0x06, 0x00, 0x00, 0x11, 0xbe, 0x40, 0x00, 0x00, 0x10, 0x00, 0x00, 0x11, 0xbe, 0x00, 0x00, 0x00, 0x00, 0x81, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-  ether.sendUdp(reply, sizeof(reply), RMCP_UDP_PORT, ip, port );
-  Serial.print("Sent a reply to ");
-  IPAddress dst(ip[0], ip[1], ip[2], ip[3]);
-  Serial.print(dst);
+
+  ether.sendUdp(reply, sizeof(reply), RMCP_UDP_PORT, srcip, port );
+  Serial.print("Sent a ASF pong to ");
+  // WHY IS THIS WRONG?
+  IPAddress replydst(srcip[0], srcip[1], srcip[2], srcip[3]);
+  Serial.print(replydst);
   Serial.print(":");
   Serial.println(port);
+  Serial.println();
+
 }
--- udp_server.ino
+++ udp_server.ino
@@ -11,9 +11,12 @@ void IPMIServer(word port, byte *ip, word srcPort, const char *data, word len) {
 
 void decodeAndReply(const char *data, word len, byte *ip, word port){
 
+  IPAddress src(ip[0], ip[1], ip[2], ip[3]);
+  Serial.print("Analyzing packet from ");
+  Serial.println(src);
+
   if (data[RMCP_HEADER_OFFSET_VERSION] == RMCP_HEADER_VERSION) {
     if (data[RMCP_HEADER_OFFSET_RESERVED] == RMCP_HEADER_RESERVED)  {
-
       respondToASF(data, len, ip, port);
     }      
 
